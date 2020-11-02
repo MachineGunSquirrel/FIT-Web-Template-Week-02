1cdcb6f4f8d64dba3286b55ea1e6c7a9500e91db
Peter Steiner <psteiner@training.local>
2020-11-02 15:58:51 -0500

    exercise 5

--- View/exercise5-content.php
+++ View/exercise5-content.php
@@ -4,11 +4,28 @@
   <div class='student-response'>
     <h1>Question #1:</h1>
     <h4>Create a keyword brainwashed script. Add an input and a button, then add the code to add functionality. 
-      The user will enter a long string of text, then on button click, the code will search the long string for a keyword and print everything after the keyword to a p tag. 
+      The user will enter a long string of text, then on button click, the code will search the long string for a keyword 
+      and print everything after the keyword to a p tag. 
       (You may use "skeleton" as the keyword if you can't think of anything else.)</h4>
     <!-- Place Answer Here -->
 
-      
+      <label for="myInput">String Input</label>
+      <input id='myInput'></input>
+      <label for="myString">String:</label>
+      <input id='myString'></input>
+      <button onclick="findString( document.getElementById('myInput').value, document.getElementById('myString').value)">Submit</button>
+      <p id='StringFound'></p>
+
+      <script>
+
+        //function that finds string and displays text after it
+        function findString(input, string)
+        {
+          var stringIndex = input.indexOf(string);
+          var stringLength = string.length;
+          document.getElementById('StringFound').innerHTML = input.substring(stringIndex+stringLength);
+        }
+      </script>
 
     <!-- Place Answer Here -->
   </div>
@@ -23,7 +40,35 @@
         (Hint two arrays or a 2 dimensional array is needed)</h4>
     <!-- Place Answer Here -->
       
+      <label for="checkInventory">Check Inventory</label>
+      <input id='checkInventory'></input>
+      <button onclick="checkInv( document.getElementById('checkInventory').value)">Check</button>
+      <p id='inventoryCount'></p><br><br><br>
 
+      <label for="ChangeInventory">Change Inventory</label>
+      <input id='ChangeInventory'></input>
+      <button onclick="changeInv( document.getElementById('ChangeInventory').value)">Check</button>
+      <p id='newInventoryCount'></p>
+
+      <script>
+
+        //array
+        var Inventory = [ ['M1A3 Abrams Main Battle Tank', 8], 
+                          ['M4A1 Carbine', 40],
+                          ['M249 Light Machine Gun', 4] ];
+        item = 0;
+          
+        function checkInv(item)
+        {
+          document.getElementById('inventoryCount').innerHTML = Inventory[item][0] + " (" + Inventory[item][1] + ")"
+        }
+
+        function changeInv(newValue)
+        {
+          Inventory[item][1] = newValue;
+          document.getElementById('newInventoryCount').innerHTML = Inventory[item][0] + " (" + Inventory[item][1] + ")"
+        }
+      </script>
 
     <!-- Place Answer Here -->
   </div>
