a49e4bdcfa83331ec7ec4dd2af98fac496e2b982
Peter Steiner <psteiner@training.local>
2020-11-04 16:33:09 -0500

    Find It

--- View/exercise6-content.php
+++ View/exercise6-content.php
@@ -6,23 +6,50 @@
     <h4>Create a dice class that holds an int attribute for the number of sides it has, and a roll function that rolls the dice and returns the outcome. 
       Add the elements and script needed so that a user can enter a number of sides and a number of times to roll; display on a button click.</h4>
     <!-- Place Answer Here -->
+
+      <label for="valSides">Number of Sides:</label>
+      <input id="valSides">
+      <label for="valRolls">Number of Rolls:</label>
+      <input id="valRolls">
+      <button onclick="questionOne(document.getElementById('valSides').value,  document.getElementById('valRolls').value)">Roll</button>
+      <p id="outcomeOne"></p>
       
-  <script>
+        <script>
 
-      class Dice
-      {
-        constructor(valSides)
-        {
-          this.sides = valSides;
-        }
+            class Dice
+            {
+              constructor(valSides)
+              {
+                this.sides = valSides;
+              }
 
-        roll()
-        {
-          result = (Math.floor(Math.random() * (sides + 1) - 1) + 1)
-        }
-      }
-  
-  </script>
+              roll()
+              {
+                //roll the dice by picking random int 1-# of sides
+                return Math.floor(Math.random() * Math.floor(this.sides)+1);
+                //print result to <p></p>
+              }
+            }
+
+            function questionOne(valSides, valRolls)
+            {
+              var resultOne = '';
+              var resultTotal = 0;
+              var die = new Dice(valSides);
+              var rollResult = 0;
+              //while loop iterating roll function until rolls variable reaches 0
+              while(valRolls > 0)
+              {
+                rollResult = die.roll();
+                resultOne += " " + rollResult;
+                resultTotal += rollResult;
+                valRolls--;
+              }
+              document.getElementById("outcomeOne").innerHTML = resultOne + ', total = ' + resultTotal;
+
+            }
+        
+        </script>
 
     <!-- Place Answer Here -->
   </div>
@@ -35,6 +62,111 @@
       (be creative and useful, it'll earn you more points! :)</h4>
     <!-- Place Answer Here -->
       
+      <label for="valSides">Number of Sides:</label>
+        <input id="valSides">
+        <label for="valRolls">Number of Rolls:</label>
+        <input id="valRolls">
+        <!-- value to be favored -->
+        <label for="weighed">Weighted value:</label>
+        <input id="weighed">
+        <!-- how heavy weight is -->
+        <label for="weight">Mass of weight:</label>
+        <input id="weight"> 
+        <button onclick="questionTwo(document.getElementById('valSides').value,  
+                                     document.getElementById('valRolls').value, 
+                                     document.getElementById('weighed').value, 
+                                     document.getElementById('weight').value)">Roll</button>
+        <p id="outcomeTwo"></p>
+        
+          <script>
+
+              class Dice
+              {
+                constructor(valSidesVal)
+                {
+                  this.sides = valSidesVal;
+                }
+
+                roll()
+                {
+                  //roll the dice by picking random int 1-# of sides
+                  return Math.floor(Math.random() * Math.floor(this.sides)+1);
+                }
+              }
+
+              function questionTwo(valSides, valRolls, weighed, weight)
+              {
+                var resultOne = '';
+                var resultTotal = 0;
+                var die = new Dice(valSides);
+                var rollResult = 0;
+                //store original value of weight
+                var weightStore = weight;
+                //boolean trip variable for weight loop
+                var weightLoop = 0;
+                //while loop iterating roll function until rolls variable reaches 0
+                while(valRolls > 0)
+                {
+                 
+                  //calls roll function from class
+                  rollResult = die.roll();
+                  //compares results to weight number and allows roll if result is favorable
+                  if (weightLoop == 0 && weight > 0 && weighed == rollResult) {
+                    //aggregates successful results
+                    resultOne += " " + rollResult;
+                    //sums successful results
+                    resultTotal += rollResult;
+                    //decrement rolls variable
+                    valRolls--;
+                  }
+                  //rerun die roll
+                  else if (weightLoop == 0 && weight > 0){
+                    //decrement weight variable
+                    weight--;
+                    console.log("Failed starting result: " + rollResult);
+                    //trip weight loop, go to else #1
+                    weightLoop = 1;
+                  }
+                  else
+                  {
+                    while (weightLoop == 1) {
+                      rollResult = die.roll();
+                      //if for successfully weighted results
+                      if (weight > 0 && weighed == rollResult) {
+                        //aggregates successful results
+                        resultOne += " " + rollResult;
+                        //sums successful results
+                        resultTotal += rollResult;
+                        //decrement rolls variable
+                        valRolls--;
+                        weightLoop = 0;
+                      }
+                      //failed result
+                      else if (weight > 0){
+                        //decrement weight
+                        weight--;
+                        console.log("Failed loop result: " + rollResult);
+                      }
+                      //weight loop fails with no successful result
+                      else if (weight == 0){
+                        //aggregates failed results on completed weight loop
+                        resultOne += " " + rollResult;
+                        //sums failed results
+                        resultTotal += rollResult;
+                        //decrement rolls variable
+                        valRolls--;
+                        weightLoop = 0;
+                      }
+                    }
+                    //reset weight variable
+                    weight = weightStore;
+                  }
+                }
+                document.getElementById("outcomeTwo").innerHTML = resultOne + ', total = ' + resultTotal;
+
+              }
+          
+          </script>
 
 
     <!-- Place Answer Here -->
